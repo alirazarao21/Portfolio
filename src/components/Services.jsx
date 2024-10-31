2f3f62e6b726131c351269bdce7fd66a8436cdf2
alirazarao21 <raoa62253@gmail.com>
2024-10-31 13:28:07 +0500

    servces

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const services =[
+const service =[
   {
     id: 1,
     title: "Web Design",
@@ -14,7 +14,7 @@ const services =[
   {
     id: 3,
     title: "React Js",
-    dercription: " Have a string grip in React js and its Concepts"
+    dercription: " Have a strong grip in React js and its Concepts"
   },
   {
     id: 4,
@@ -29,7 +29,17 @@ const Services = () => {
       <div className='container mx-auto px-8 md:px-16 lg:px-24'>
         <h2 className='text-4xl font-bold text-center mb-12'>My services</h2>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
-
+            {service.map(service =>(
+              <div key={service.id}
+              className='bg-gray-800 px-6 pb-6 rounded-lg hover:shadow-lg transform transition-transform duration-300 hover:scale-105'>
+                  <div className='text-right text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-600 to-blue-400'>
+                    {service.id}
+                  </div>
+                  <h3 className='mt-2 text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-600 to-blue-400'>{service.title}</h3>
+                  <p className='mt-2 text-gray-300'>{service.dercription}</p>
+                  <a href="#" className='mt-4 inline-block text-green-400 hover:text-blue-500'>Read More </a>
+              </div>
+            ))}
         </div>
       </div>
     </div>
